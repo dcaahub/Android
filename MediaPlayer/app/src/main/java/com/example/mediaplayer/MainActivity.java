4737632f14eba0bb2f8cf7a5451b708d2787b637
Danillo <Danillo>
2020-02-23 23:46:37 -0400

    implementado controle de volume

--- MediaPlayer/app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ MediaPlayer/app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
+    private SeekBar seekVolume;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +21,72 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+        inicializarSeekBar();
+
+    }
+
+    public void inicializarSeekBar(){
+        seekVolume = findViewById(R.id.seekVolume);
+
+        //configura audio manager
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        //recupera valores do volume maximo e atual
+        int volumeMaximo = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int volumeAtual = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        //configurar valor maximo na seekbar
+        seekVolume.setMax(volumeMaximo);
+
+        //configura progresso atual da seekbar
+        seekVolume.setProgress(volumeAtual);
+
+        seekVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
     }
 
     public void executarSom(View view){
         if(mediaPlayer != null){
             mediaPlayer.start();
+
+        }
+
+    }
+
+    public void pausarSom(View view){
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        }
+    }
+
+    public void pararSom(View view){
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
         }
     }
 }
