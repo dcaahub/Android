289ee55276b0ecee4dc9bb8cd30ce47a007943e8
DCAA <dcaa@outlook.com.br>
2020-05-24 18:28:28 -0400

    Executando sons

--- AprendaIngles/app/src/main/java/com/example/aprendaingles/Fragment/AnimalFragment.java
+++ AprendaIngles/app/src/main/java/com/example/aprendaingles/Fragment/AnimalFragment.java
@@ -1,19 +1,25 @@
 package com.example.aprendaingles.Fragment;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.aprendaingles.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AnimalFragment extends Fragment {
+public class AnimalFragment extends Fragment implements View.OnClickListener {
+
+    private ImageButton buttonCao, buttonGato, buttonLeao, buttonMacaco, buttonOvelha, buttonVaca;
+    private MediaPlayer mediaPlayer;
 
     public AnimalFragment() {
         // Required empty public constructor
@@ -24,6 +30,75 @@ public class AnimalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_animal, container, false);
+        View view = inflater.inflate(R.layout.fragment_animal, container, false);
+
+        buttonCao = view.findViewById(R.id.buttonCao);
+        buttonGato = view.findViewById(R.id.buttonGato);
+        buttonLeao = view.findViewById(R.id.buttonLeao);
+        buttonMacaco = view.findViewById(R.id.buttonMacaco);
+        buttonOvelha = view.findViewById(R.id.buttonOvelha);
+        buttonVaca = view.findViewById(R.id.buttonVaca);
+
+        buttonCao.setOnClickListener(this);
+        buttonGato.setOnClickListener(this);
+        buttonLeao.setOnClickListener(this);
+        buttonMacaco.setOnClickListener(this);
+        buttonOvelha.setOnClickListener(this);
+        buttonVaca.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.buttonCao :
+                tocarSom(R.raw.dog);
+                break;
+
+            case R.id.buttonGato :
+                tocarSom(R.raw.cat);
+                break;
+
+            case R.id.buttonLeao :
+                tocarSom(R.raw.lion);
+                break;
+
+            case R.id.buttonMacaco :
+                tocarSom(R.raw.monkey);
+                break;
+
+            case R.id.buttonOvelha :
+                tocarSom(R.raw.sheep);
+                break;
+
+            case R.id.buttonVaca :
+                tocarSom(R.raw.cow);
+                break;
+        }
+    }
+
+    public void tocarSom(int som){
+        mediaPlayer = MediaPlayer.create(getActivity(), som);
+
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
--- AprendaIngles/app/src/main/java/com/example/aprendaingles/Fragment/NumeroFragment.java
+++ AprendaIngles/app/src/main/java/com/example/aprendaingles/Fragment/NumeroFragment.java
@@ -1,5 +1,6 @@
 package com.example.aprendaingles.Fragment;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.aprendaingles.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NumeroFragment extends Fragment {
+public class NumeroFragment extends Fragment implements View.OnClickListener{
+    private ImageButton buttonUm, buttonDois, buttonTres, buttonQuatro, buttonCinco, buttonSeis;
+    private MediaPlayer mediaPlayer;
 
     public NumeroFragment() {
         // Required empty public constructor
@@ -24,6 +28,77 @@ public class NumeroFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numero, container, false);
+        View view = inflater.inflate(R.layout.fragment_animal, container, false);
+
+        buttonUm = view.findViewById(R.id.buttonUm);
+        buttonDois = view.findViewById(R.id.buttonDois);
+        buttonTres = view.findViewById(R.id.buttonTres);
+        buttonQuatro = view.findViewById(R.id.buttonQuatro);
+        buttonCinco = view.findViewById(R.id.buttonCinco);
+        buttonSeis = view.findViewById(R.id.buttonSeis);
+
+        buttonUm.setOnClickListener(this);
+        buttonDois.setOnClickListener(this);
+        buttonTres.setOnClickListener(this);
+        buttonQuatro.setOnClickListener(this);
+        buttonCinco.setOnClickListener(this);
+        buttonSeis.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()){
+            case R.id.buttonUm :
+                tocarSom(R.raw.one);
+                break;
+
+            case R.id.buttonDois :
+                tocarSom(R.raw.two);
+                break;
+
+            case R.id.buttonTres :
+                tocarSom(R.raw.three);
+                break;
+
+            case R.id.buttonQuatro :
+                tocarSom(R.raw.four);
+                break;
+
+            case R.id.buttonCinco :
+                tocarSom(R.raw.five);
+                break;
+
+            case R.id.buttonSeis :
+                tocarSom(R.raw.six);
+                break;
+        }
+
+    }
+
+    public void tocarSom(int som){
+        mediaPlayer = MediaPlayer.create(getActivity(), som);
+
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
