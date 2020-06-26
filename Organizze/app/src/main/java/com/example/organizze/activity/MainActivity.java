957fdddc44758739514754bf3f77ea71d343172b
DCAA <dcaa@outlook.com.br>
2020-06-26 12:54:57 -0400

    Implementado cadastro de usuário

--- Organizze/app/src/main/java/com/example/organizze/activity/MainActivity.java
+++ Organizze/app/src/main/java/com/example/organizze/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.organizze;
+package com.example.organizze.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.organizze.R;
 import com.example.organizze.activity.CadastroActivity;
 import com.example.organizze.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
