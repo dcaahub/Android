ecfc84705b0a44d620653c06d415ed3d18eedb4b
DCAA <dcaa@outlook.com.br>
2020-06-26 22:01:23 -0400

    Implementado validação e autenticação de usuário

--- Organizze/app/src/main/java/com/example/organizze/activity/CadastroActivity.java
+++ Organizze/app/src/main/java/com/example/organizze/activity/CadastroActivity.java
@@ -82,7 +82,7 @@ public class CadastroActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if( task.isSuccessful() ){
-                    Toast.makeText(CadastroActivity.this, "Sucesso ao cadastrar usuário", Toast.LENGTH_SHORT).show();
+                    finish();
                 }
                 else{
                     String excecao = "";
--- Organizze/app/src/main/java/com/example/organizze/activity/MainActivity.java
+++ Organizze/app/src/main/java/com/example/organizze/activity/MainActivity.java
@@ -9,11 +9,15 @@ import android.view.View;
 import com.example.organizze.R;
 import com.example.organizze.activity.CadastroActivity;
 import com.example.organizze.activity.LoginActivity;
+import com.example.organizze.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class MainActivity extends IntroActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,13 +57,31 @@ public class MainActivity extends IntroActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        verificarUsuarioLogado();
+    }
+
     public void btEntrar(View view){
         startActivity(new Intent(this, LoginActivity.class));
-
     }
 
     public void btCadastrar(View view){
         startActivity(new Intent(this, CadastroActivity.class));
+    }
 
+    public void verificarUsuarioLogado(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        if(autenticacao.getCurrentUser() != null ){
+            abrirTelaPrincipal();
+        }
+
+    }
+
+    public void abrirTelaPrincipal(){
+        startActivity(new Intent(this, PrincipalActivity.class));
+        finish();
     }
 }
