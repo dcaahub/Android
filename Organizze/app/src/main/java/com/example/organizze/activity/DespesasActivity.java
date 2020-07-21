a90d8c865b1768e51129d1c9001d5065366845d8
DCAA <dcaa@outlook.com.br>
2020-07-21 13:56:37 -0400

    Salvando Despesa

--- Organizze/app/src/main/java/com/example/organizze/activity/DespesasActivity.java
+++ Organizze/app/src/main/java/com/example/organizze/activity/DespesasActivity.java
@@ -3,15 +3,18 @@ package com.example.organizze.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.example.organizze.R;
 import com.example.organizze.helper.DateCustom;
+import com.example.organizze.model.Movimentacao;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class DespesasActivity extends AppCompatActivity {
     private TextInputEditText campoData, campoCategoria, campoDescricao;
     private EditText campoValor;
+    private Movimentacao movimentacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +28,18 @@ public class DespesasActivity extends AppCompatActivity {
 
         //preenche o campo com a data atual
         campoData.setText(DateCustom.dataAtual());
+    }
+
+    public void salvarDespesa(View view){
+        movimentacao = new Movimentacao();
+        String data = campoData.getText().toString();
+        movimentacao.setValor(Double.parseDouble(campoValor.getText().toString()));
+        movimentacao.setCategoria(campoCategoria.getText().toString());
+        movimentacao.setDescricao(campoDescricao.getText().toString());
+        movimentacao.setTipo("d");
+        movimentacao.setData(data);
+
+        movimentacao.salvar(data);
+
     }
 }
--- Organizze/app/src/main/java/com/example/organizze/helper/DateCustom.java
+++ Organizze/app/src/main/java/com/example/organizze/helper/DateCustom.java
@@ -10,4 +10,15 @@ public class DateCustom {
         String dataString = simpleDateFormat.format(date);
         return dataString;
     }
+
+    public static String mesAnoDataEscolhida(String data){
+        String retornoData[] = data.split("/");
+        String dia = retornoData[0];
+        String mes = retornoData[1];
+        String ano = retornoData[2];
+        String mesAno = mes+ano;
+
+        return mesAno;
+
+    }
 }
