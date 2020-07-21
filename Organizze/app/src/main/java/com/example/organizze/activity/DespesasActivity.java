8f5f047aa24ba6005c67af496d0418c096af1f28
DCAA <dcaa@outlook.com.br>
2020-07-21 12:20:09 -0400

    Exibição data atual no campo de data

--- Organizze/app/src/main/java/com/example/organizze/activity/DespesasActivity.java
+++ Organizze/app/src/main/java/com/example/organizze/activity/DespesasActivity.java
@@ -3,14 +3,27 @@ package com.example.organizze.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.organizze.R;
+import com.example.organizze.helper.DateCustom;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class DespesasActivity extends AppCompatActivity {
+    private TextInputEditText campoData, campoCategoria, campoDescricao;
+    private EditText campoValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_despesas);
+
+        campoData = findViewById(R.id.editData);
+        campoCategoria = findViewById(R.id.editCategoria);
+        campoDescricao = findViewById(R.id.editDescricao);
+        campoValor = findViewById(R.id.editValor);
+
+        //preenche o campo com a data atual
+        campoData.setText(DateCustom.dataAtual());
     }
 }
